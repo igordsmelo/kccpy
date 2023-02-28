f2566477dd95a6234b0fbb59b0c0ec7c3fd71fdc
igordsmelo <102251050+igordsmelo@users.noreply.github.com>
2023-02-28 20:48:16 -0300

    organizing code. Expect things to break.

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ from send2trash import send2trash as trash
 from os import listdir
 from shutil import copy
 from time import sleep
-from utils.windows_tools import notification #todo: import this from DailyTasks
+
+import sys
+
+sys.path.append(r'D:\Users\Igor\Documents\MEGA\Programming\Projects\DailyTasks')
+
+from utils.windows_tools import notification  # todo: import this from DailyTasks
 
 
 ############################################################ RUN THIS FIRST, TO GET THE FILES AND FOLDERS TO CONVERT
@@ -41,20 +46,28 @@ def send_to_kindle(file, kindle_dir="E:\\", delete_original=True):
         notification('file not found')
         pass
 
-
-import os
-path = r"D:\Users\Igor\Documents\Sync\Manga"
-paths = [f"{path}\\{p}" for p in os.listdir(path) if os.path.isdir(f"{path}\\{p}")]
-for p in paths:
-    for manga in os.listdir(p):
-        pth = f"{p}\\{manga}"
-        if os.path.isdir(pth):
-            chapters = os.listdir(pth)
-            chapters.sort()  # tries to sort list alphabetically. Doesn't work perfectly. #todo: fix it.
-            mobi_name = f"{manga} {chapters[0]}-{chapters[-1]}"
-            if any('cbz' in c for c in chapters) or any('_tmp' in c for c in chapters):
-                print(f'haha {manga} is wrong')
-            else:
-                print(f'converting {manga}')
-                make_manga(pth, fname=mobi_name)
 ############################################################
+
+def main():
+    from os.path import isdir  # checks if folder exists.
+    from os import listdir  # gets files in dir.
+    path = r"D:\Users\Igor\Documents\Sync\Manga"
+    paths = [f"{path}\\{p}" for p in listdir(path) if isdir(f"{path}\\{p}")]
+    for p in paths:
+        for manga in listdir(p):
+            pth = f"{p}\\{manga}"
+            if isdir(pth):
+                chapters = listdir(pth)
+                chapters.sort()  # tries to sort list alphabetically. Doesn't work perfectly. #todo: fix it.
+                mobi_name = f"{manga} {chapters[0]}-{chapters[-1]}"
+                if any('cbz' in c for c in chapters) or any('_tmp' in c for c in chapters):
+                    print(f'haha {manga} is wrong')
+                else:
+                    print(f'converting {manga}')
+                    make_manga(pth, fname=mobi_name)
+
+
+############################################################
+
+if __name__ == '__main__':
+    main()
