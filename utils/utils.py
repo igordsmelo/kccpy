baea99d9396b43532efb2919c1d610fa648c84d7
Igor <igordsmelo@gmail.com>
2023-09-23 16:39:43 -0300

    refactoring
    Signed-off-by: Igor <igordsmelo@gmail.com>

--- utils/utils.py
+++ utils/utils.py
@@ -6,7 +6,7 @@ from time import sleep
 from send2trash import send2trash as trash
 
 
-# sys.path.append(r'D:\Users\Igor\Documents\MEGA\Programming\Projects\DailyTasks')
+# sys.path.append(r'D:\Users\Igor\Documents\MEGA\Programming\Projects\0')
 
 ############################################################ RUN THIS FIRST, TO GET THE FILES AND FOLDERS TO CONVERT
 def list_manga_files(fldr_pth, exclude_format='MOBI') -> list:
