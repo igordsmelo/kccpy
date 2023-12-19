7a01e205b6d19cdcb5d4ef4af99f1cb9332cb1e5
Igor <igordsmelo@gmail.com>
2023-12-19 18:43:16 -0300

    first release

---
+++ main.py
@@ -0,0 +1,9 @@
+import kccpy.utils as kcc
+
+def main():
+    file = input('Paste path to file here')
+    kcc.make_comic(file)
+    print('Conversion finished. Enjoy your reading!')
+
+if __name__ == '__main__':
+    main()
